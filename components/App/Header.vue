03736cc21354082ebab1620dc7766afede10c4a0
joshytheprogrammer <jurassicjoshua@protonmail.com>
2025-04-18 22:37:48 +0100

    made changes

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -3,7 +3,7 @@
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between items-center">
     <div class="max-w-7xl px-4 py-4 sm:px-6 lg:px-8 flex gap-4 cursor-pointer" @click="$router.push('/a')">
       <AppLogo class="h-10 w-auto" />
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h1 class="hidden md:block text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
         Operation Wooden Tree
       </h1>
     </div>
